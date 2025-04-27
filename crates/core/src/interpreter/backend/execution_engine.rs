704e0793d11937cd1b8eece5f7ff0c9c41cd1ebe
Angry Coding <mailidanielifechukwu@gmail.com>
2025-04-27 23:28:03 +0000

    chore: resolvers

---
+++ crates/core/src/interpreter/backend/execution_engine.rs
@@ -0,0 +1,65 @@
+use super::{
+    resolve_account::resolve_account_query, resolve_block::resolve_block_query,
+    resolve_logs::resolve_log_query, resolve_transaction::resolve_transaction_query,
+};
+use crate::common::{
+    entity::Entity,
+    query_result::{ExpressionResult, QueryResult},
+    serializer::dump_results,
+    types::{Expression, GetExpression},
+};
+use anyhow::Result;
+
+pub struct ExecutionEngine;
+
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+pub enum ExecutionEngineError {
+    #[error("Neither an entity_id nor a filter was provided. Pest rules should have prevented this from happening.")]
+    NoEntityIdOrFilter,
+    #[error("Multiple filters are not supported for block queries.")]
+    MultipleFiltersNotSupported,
+}
+
+impl ExecutionEngine {
+    pub fn new() -> ExecutionEngine {
+        ExecutionEngine
+    }
+
+    pub async fn run(&self, expressions: Vec<Expression>) -> Result<Vec<QueryResult>> {
+        let mut query_results = vec![];
+
+        for expression in expressions {
+            match expression {
+                Expression::Get(get_expr) => {
+                    let result = self.run_get_expr(&get_expr).await?;
+                    query_results.push(QueryResult::new(result));
+                }
+            }
+        }
+
+        Ok(query_results)
+    }
+
+    async fn run_get_expr(&self, expr: &GetExpression) -> Result<ExpressionResult> {
+        let result = match &expr.entity {
+            Entity::Block(block) => {
+                ExpressionResult::Block(resolve_block_query(block, &expr.chains).await?)
+            }
+            Entity::Account(account) => {
+                ExpressionResult::Account(resolve_account_query(account, &expr.chains).await?)
+            }
+            Entity::Transaction(transaction) => ExpressionResult::Transaction(
+                resolve_transaction_query(transaction, &expr.chains).await?,
+            ),
+            Entity::Logs(logs) => {
+                ExpressionResult::Log(resolve_log_query(logs, &expr.chains).await?)
+            }
+        };
+
+        if let Some(dump) = &expr.dump {
+            let _ = dump_results(&result, dump);
+        }
+
+        Ok(result)
+    }
+}
--- crates/core/src/interpreter/backend/mod.rs
+++ crates/core/src/interpreter/backend/mod.rs
@@ -1,5 +1,5 @@
+pub mod execution_engine;
 mod resolve_account;
 mod resolve_checkpoint;
 mod resolve_event;
 mod resolve_transaction;
-pub mod execution_engine;
